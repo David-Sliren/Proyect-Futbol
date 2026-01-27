46db44d16e38e8f58e4324554461dbcaea8eafaa
David Sliren <bluee.97.berry@gmail.com>
2026-01-27 00:51:18 -0500

    feact(Context): implementar guardado de id en local storage,  de cada competicion a la que se le halla hecho click

--- frontend/src/context/boton_data/BotonData.jsx
+++ frontend/src/context/boton_data/BotonData.jsx
@@ -1,9 +1,19 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 const ContextBotones = createContext();
 const KEY_STORE = "competition-store";
 function BotonData({ children }) {
-  const [botonData, setBotonData] = useState(null);
+  const [botonData, setBotonData] = useState(getDataStore);
+
+  function getDataStore() {
+    const data = localStorage.getItem(KEY_STORE);
+    return data ? JSON.parse(data) : null;
+  }
+
+  useEffect(() => {
+    if (!botonData) return;
+    localStorage.setItem(KEY_STORE, JSON.stringify(botonData));
+  }, [botonData]);
 
   function getData(id) {
     setBotonData(id);

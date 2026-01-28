042c5951373efd75713a982e2d6deef0fbe11d57
David Sliren <bluee.97.berry@gmail.com>
2026-01-27 22:30:45 -0500

    feact(backend/server): Implementar cache a la ruta matches y table

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,14 +21,19 @@ const ligaConfig = {
   bigfive: bigFive,
   sudamerican: sudamerican,
 };
+const typeCompetition = ["LEAGUE", "CUP"];
 
 const cacheTypeCompetitions = {
   LEAGUE: { data: null, lastTime: 0 },
   CUP: { data: null, lastTime: 0 },
-  duration: 60 * 1000,
+  duration: 1 * 60 * 60 * 1000,
 };
 
-const typeCompetition = ["LEAGUE", "CUP"];
+const cacheMatches = {};
+const CACHE_DURATION_Matches = 120 * 1000;
+
+const cacheTable = {};
+const CACHE_DURATION_Table = 5 * 60 * 1000;
 
 app.use(cors());
 app.use(morgan("dev")); // Logger de peticiones
@@ -41,34 +46,29 @@ app.use("/", express.static("public"));
 app.get("/api/competitions/:type", async (req, res) => {
   const { type } = req.params;
   const { priority } = req.query;
-
-  // Para aceder a los los tipos de params en el cache
-  const currentCompetitions = cacheTypeCompetitions[type];
-
-  // Tiempo para calcular el guardado en cahe
   const NOW = Date.now();
-  const LAST_TIME = NOW - cacheTypeCompetitions.lastTime;
 
   if (!typeCompetition.includes(type)) {
     return res.status(400).json({
       error: `Error Tipo de competicion no valida elija ${typeCompetition[0]} o ${typeCompetition[1]}`,
     });
   }
+  // Para aceder a los los tipos de params en el cache
+  const current = cacheTypeCompetitions[type];
 
+  // Tiempo para calcular el guardado en cahe
+  const DIFF = NOW - current.lastTime;
   try {
-    if (
-      !currentCompetitions.data ||
-      LAST_TIME > cacheTypeCompetitions.duration
-    ) {
+    if (!current.data || DIFF > cacheTypeCompetitions.duration) {
       const fecthData = await fetchCompetitions();
       const filterType = fecthData
         .filter((t) => t.type === type)
         .map((L) => ({ id: L.id, name: L.name, logo: L.emblem, type: L.type }));
-      currentCompetitions.data = filterType;
-      currentCompetitions.lastTime = NOW;
+      current.data = filterType;
+      current.lastTime = NOW;
     }
 
-    const data = currentCompetitions.data;
+    const data = current.data;
 
     // filtras las 5 grade ligas y las ligas sudamericanas
     if (priority) {
@@ -85,14 +85,27 @@ app.get("/api/competitions/:type", async (req, res) => {
 
 app.get("/api/competitions/:id/table", async (req, res) => {
   const { id } = req.params;
+  const NOW = Date.now();
+
+  if (cacheTable[id] && NOW - cacheTable[id].lastTime < CACHE_DURATION_Table) {
+    {
+      return res.json(cacheTable[id].data);
+    }
+  }
 
   try {
     const data = await fetchCompetitionsId(id);
-    if (data.length === 0) {
-      return res.json([]);
-    }
-    const filterTable = data[0].table;
-    res.json(filterTable);
+
+    if (data.length === 0) return res.json([]);
+
+    const clean = data[0].table;
+
+    cacheTable[id] = {
+      data: clean,
+      lastTime: NOW,
+    };
+
+    res.json(clean);
   } catch (e) {
     res.status(500).json({ error: "Error al obtener datos de futbol" });
   }
@@ -100,6 +113,8 @@ app.get("/api/competitions/:id/table", async (req, res) => {
 
 app.get("/api/matches", async (req, res) => {
   const { competitions } = req.query;
+  const id = competitions || "all";
+  const NOW = Date.now();
 
   function filterMatch(e) {
     const data = e?.map((m) => ({
@@ -114,15 +129,26 @@ app.get("/api/matches", async (req, res) => {
     return data;
   }
 
-  try {
-    if (competitions) {
-      const data = await matchesCompetition(competitions);
-      if (data.length === 0) return res.json([]);
-      res.json(filterMatch(data));
-    }
+  if (
+    cacheMatches[id] &&
+    NOW - cacheMatches[id].lastTime < CACHE_DURATION_Matches
+  ) {
+    return res.json(cacheMatches[id].data);
+  }
 
-    const data = await fetchMatches();
-    res.json(filterMatch(data));
+  try {
+    const data = competitions
+      ? await matchesCompetition(id)
+      : await fetchMatches();
+    if (!data || data.length === 0) return res.json([]);
+    const clean = filterMatch(data);
+
+    cacheMatches[id] = {
+      data: clean,
+      lastTime: NOW,
+    };
+
+    res.json(clean);
   } catch (error) {
     res.status(500).json({ error: "Error al obtener datos de fútbol" });
   }
